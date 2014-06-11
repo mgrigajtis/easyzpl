95d1e1bab8b82e0aa5cba504f1a4b0272381012e
Matthew Grigajtis <matthew.grigajtis@ctg.com>
2014-06-11 09:06:57 -0400

    Adds a stored label object

---
+++ lib/easyzpl/stored_label.rb
@@ -0,0 +1,8 @@
+require 'label'
+
+# This module is a wrapper for writing confusing ZPL and ZPL2 code
+module Easyzpl
+  # This is the stored label object
+  class StoredLabel < Easyzpl::Label
+  end
+end
